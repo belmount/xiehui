3075c49960122bca92fd1fb53d0fa1138d671d65
feng bai <feng.bai.cn@gmail.com>
2014-06-20 23:02:19 +0800

    fix rails 4 i18n bug in production

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Zjxh
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    I18n.config.enforce_available_locales = false
     config.i18n.default_locale = 'zh-CN'
   end
 end
